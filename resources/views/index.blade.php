d632b0c30d948d367bf193cbfcb3814eb95f677d
DESKTOP-LLN0I6B\n0br41n <michael4720@msn.com>
2018-11-08 01:41:36 +0100

    aaa

---
+++ resources/views/index.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Test</h1>
+
+<div class="container">
+
+
+    <div class="row product-container">
+      <div class="col-md-3 product-item">
+        <div class="product-image">
+          <img src="https://i.pinimg.com/originals/63/72/05/6372058fc6082594c9b33756546f2be8.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="product-title">
+          Title Name
+        </div>
+
+        <div class="product-purchase-details">
+
+          <div class="product-price">
+            Price-tag
+          </div>
+          <div class="product-cart">
+            cart to cart
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-3 product-item">
+        <div class="product-image img-fluid">
+          <img src="https://i.pinimg.com/originals/63/72/05/6372058fc6082594c9b33756546f2be8.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="product-title">
+          Title Name
+        </div>
+
+        <div class="product-purchase-details">
+
+          <div class="product-price">
+            Price-tag
+          </div>
+          <div class="product-cart">
+            cart to cart
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-3 product-item">
+        <div class="product-image img-fluid">
+          <img src="https://i.pinimg.com/originals/63/72/05/6372058fc6082594c9b33756546f2be8.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="product-title">
+          Title Name
+        </div>
+
+        <div class="product-purchase-details">
+
+          <div class="product-price">
+            Price-tag
+          </div>
+          <div class="product-cart">
+            cart to cart
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-3 product-item">
+        <div class="product-image img-fluid">
+          <img src="https://i.pinimg.com/originals/63/72/05/6372058fc6082594c9b33756546f2be8.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="product-title">
+          Title Name
+        </div>
+
+        <div class="product-purchase-details">
+
+          <div class="product-price">
+            Price-tag
+          </div>
+          <div class="product-cart">
+            cart to cart
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-3 product-item">
+        <div class="product-image img-fluid">
+          <img src="https://i.pinimg.com/originals/63/72/05/6372058fc6082594c9b33756546f2be8.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="product-title">
+          Title Name
+        </div>
+
+        <div class="product-purchase-details">
+
+          <div class="product-price">
+            Price-tag
+          </div>
+          <div class="product-cart">
+            cart to cart
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-3 product-item">
+        <div class="product-image img-fluid">
+          <img src="https://i.pinimg.com/originals/63/72/05/6372058fc6082594c9b33756546f2be8.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="product-title">
+          Title Name
+        </div>
+
+        <div class="product-purchase-details">
+
+          <div class="product-price">
+            Price-tag
+          </div>
+          <div class="product-cart">
+            cart to cart
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</body>
+</html>
